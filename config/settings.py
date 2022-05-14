e9f45383df4a603f57ebe8122bdc511b742e3338
kjs2109 <kimjusung2109@naver.com>
2022-05-14 21:27:44 +0900

    홈페이지 구현

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'home',
     'users',
     'allauth',
     'allauth.account',
