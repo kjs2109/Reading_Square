b08fb57819d6d1a2ce238976816526a42fa445e8
kjs2109 <kimjusung2109@naver.com>
2022-05-21 18:55:35 +0900

    footer 바닥에 고정, 화면 크기에 맞게 메인베너 크기 조정

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(-&%rzszznti2rq)ox=^kk%iwjux@u**k1g)36#&2r32h03kb5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- static/home/js/index.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.navbar-toggle');
-const menu = document.querySelector('#menu');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
